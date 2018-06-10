794c0af719e1570493fdc4374e0ae2f7df282983
Britton Walker <bwalker1801@gmail.com>
2018-06-09 22:19:10 -0400

    Adding yt player

--- js/main.js
+++ js/main.js
@@ -9,31 +9,44 @@ import SetMobileHeight from './app/SetMobileHeight';
 import './app/MobileSlideshow';
 import './app/Homepage';
 
-// var rellax = new Rellax('.rellax', {
-//     center: true
-// });
-
 document.addEventListener("DOMContentLoaded", function (event) {
     initSections('[data-animate]');
 });
 
 
-$('.mobile-vid-trigger').on('click', function(e) {
-    e.preventDefault();
-})
-
-// Inject YouTube API script
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-$(document).ready(function() {
-    $('.mobile-vid-trigger').on('click', function(ev) {
-
-        console.log('clicked');
-        $("#video")[0].src += "&autoplay=1";
-        ev.preventDefault();
-
+var YouTubeIframeLoader = require('youtube-iframe');
+$(document).ready( function() {
+    var player;
+    YouTubeIframeLoader.load(function(YT) {
+        player = new YT.Player('player', {
+            playerVars: {
+                modestbranding: true
+              },
+            height: '390',
+            width: '640',
+            videoId: 'QReEdfjUqXw',
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
     });
-});
+
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+    var done = false;
+    function onPlayerStateChange(event) {
+        if(event.data === 0) {          
+            $('.mobile-vid-frame').removeClass('active');
+        }        
+    }
+    function stopVideo() {
+        player.stopVideo();
+    }
+
+    $('.mobile-vid-trigger').on('click', function(e) {
+        e.preventDefault();
+        $('.mobile-vid-frame').addClass('active');
+        player.playVideo();
+    })
+})
