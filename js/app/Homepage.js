353b388dbfffe1131aab489438de11cc1f5638e2
Britton Walker <bwalker1801@gmail.com>
2018-06-17 22:19:25 -0400

    More changes.

--- js/app/Homepage.js
+++ js/app/Homepage.js
@@ -53,26 +53,26 @@ class Homepage {
         var $imgs = $('.testing img').toArray().reverse();
 
         var textArray = [{
-                text: 'A sound meditation app',
+                text: 'An evolved<br>sound meditation app',
                 color: '#3FCCD9',
                 logoColor: '#FAF6ED'
             },
             {
-                text: 'designed to harmonize sense and self.',
+                text: 'designed to harmonize<br>sense and self.',
                 color: '#7268E3'
             },
             {
-                text: 'Responsive to your environment and conscious of your intention.',
+                text: 'Responsive to your<br>environment and conscious<br>of your intention.',
                 color: '#FF6417',
                 logoColor: '#CCFBD0'
             },
             {
-                text: 'No two sessions are alike, each soundscape is generated note by note.',
+                text: 'No two sessions are alike,<br>each soundscape is<br>generated note by note.',
                 color: '#A09FD1',
                 logoColor: '#BF4854'
             },
             {
-                text: 'Minimal, adaptive, and infinite.',
+                text: 'Minimal, adaptive,<br>and infinite.',
                 color: '#5465DD'
             },
         ]
